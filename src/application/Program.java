d08ebf4eca2d00cbbe3c33b175eb8956bccef720
Cesar <cesar.socin@gmail.com>
2023-11-29 15:58:47 -0200

    delete implamentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -17,14 +18,14 @@ public class Program {
 		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println("==== 1 teste");
+		System.out.println("==== 1 teste-fidnID");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
-		System.out.println("==== 2 teste");
+		System.out.println("==== 2 seller-findDepart");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
@@ -32,7 +33,7 @@ public class Program {
 			
 		}
 
-		System.out.println("==== 3 teste");
+		System.out.println("==== 3 seller-findAll");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
@@ -40,17 +41,24 @@ public class Program {
 		}
 
 		
-		System.out.println("==== 4 teste-insert");
+		System.out.println("==== 4 seller-insert");
 		Seller newSwller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSwller);
 		System.out.println("Insert new id: " + newSwller.getId());
 		
-		System.out.println("==== 5 teste-update");
-		seller = sellerDao.findById(1);
+		System.out.println("==== 5 seller-update");
+		seller = sellerDao.findById(2);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("==== Update comleto");
+
+		System.out.println("==== 6 seller-delete");
+		System.out.println("Digite o codigo a ser excluido: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletado...");
 		
+		sc.close();
 	}
 
 }
